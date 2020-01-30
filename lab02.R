e8462e3707b0d1891b08525de2a5dd82a1840f92
Brendan Zuk <zukbrend@msu.edu>
2020-01-29 20:30:59 -0500

    Exercise 2 code, just needs the writing finished

--- lab02.R
+++ lab02.R
@@ -14,10 +14,8 @@ unique(ameslist$GarageType)
 
 # Create a matrix to transform GarageType factor data into numeric data
 # https://stackoverflow.com/questions/5616210/model-matrix-with-na-action-null
-current.na.action <- options('na.action')
 options(na.action='na.pass')
 GarageTemp = model.matrix(~ GarageType - 1, data=ameslist)
-options(na.action=current.na.action)
 
 # Combine the numeric matrix and full data frame together
 ameslist <- cbind(
@@ -90,4 +88,85 @@ beta_0_hat = mean(y) - beta_1_hat * mean(x)
 
 abline(beta_0_hat, beta_1_hat, col="blue")
 
+################################################################################
 
+
+
+################################################################################
+
+### 1 ###
+# Create the GarageInside column in ameslist
+ameslist$GarageInside <- ifelse(
+  ameslist$GarageTypeDetchd == 0 & ameslist$GarageTypeCarPort == 0, 
+  1, 
+  0
+)
+
+# Check for any NA values in GarageOutside, and change them to a 0
+ameslist$GarageInside <- ifelse(
+  is.na(ameslist$GarageInside),
+  0,
+  ameslist$GarageInside
+)
+
+indoorGarageSLR <- lm(
+  SalePrice ~ GarageInside - 1,
+  data=ameslist
+)
+summary(indoorGarageSLR)
+
+### 2 ###
+options(na.action='na.omit')
+
+fullAmesModel <- lm(
+  SalePrice ~ .,
+  data=Ames
+)
+summary(fullAmesModel)
+
+### 3 ###
+
+# https://data.library.virginia.edu/diagnostic-plots/
+par(mfrow=c(2,2)) # Change the panel layout to 2 x 2
+plot(fullAmesModel)
+par(mfrow=c(1,1)) # Change back to 1 x 1
+
+### 4 ###
+fullAmesModel2 <- lm(
+  SalePrice ~ . + LotArea:BedroomAbvGr,
+  data=Ames
+)
+summary(fullAmesModel2)
+
+fullAmesModel3 <- lm(
+  SalePrice ~ . + BedroomAbvGr:TotRmsAbvGrd,
+  data=Ames
+)
+summary(fullAmesModel3)
+
+fullAmesModel4 <- lm(
+  SalePrice ~ . + YearBuilt*YrSold,
+  data=Ames
+)
+summary(fullAmesModel4)
+
+### 5 ###
+
+fullAmesModel5 <- lm(
+  SalePrice ~ . - LotArea + log2(LotArea),
+  data=Ames
+)
+summary(fullAmesModel5)
+# Lot Area became more significant when log2'd
+
+fullAmesModel6 <- lm(
+  SalePrice ~ . + (OverallCond)^2 + (OverallQual)^2,
+  data=Ames
+)
+summary(fullAmesModel6)
+
+fullAmesModel7 <- lm(
+  SalePrice ~ . + sqrt(BedroomAbvGr),
+  data=Ames
+)
+summary(fullAmesModel7)
